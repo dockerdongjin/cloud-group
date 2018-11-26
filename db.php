c858f14a1303f77e27b4dbacc43b3943bd1fb969
fmrk98 <fmrk98@gmail.com>
2018-11-26 15:39:42 +0900

    no message

---
+++ db.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+</head>
+<body>
+<h1>Hello PHP</h1>
+<table>
+<?php
+	$conn = mysqli_connect(
+	  'localhost',
+	  'cloud',
+	  'password',
+	  'cloud');
+	$sql = "SELECT * FROM users";
+	$result = mysqli_query($conn, $sql);
+	while($row = mysqli_fetch_array($result)) {
+		echo '<tr>';
+		for ($i = 0;$i < count($row);$i++) {
+			echo '<td>'.$row[$i].'</td>';
+		}
+		echo '</tr>';
+	}
+?>
+</table>
+</body>
+</html>
