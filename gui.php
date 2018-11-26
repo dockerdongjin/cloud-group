2693a4f01539eb2e6e7dd44e136f42da38e816c9
Dongjin Oh <csco0130@gmail.com>
2018-11-26 04:09:53 +0000

    final gui.php added

--- gui.php
+++ gui.php
@@ -3,7 +3,6 @@
 	echo "starhj10@gmail.com";
 	echo "csco0130@gmail.com";
    	echo "fmrk98@gmail.com";
-             echo "csco013022@gmail.com";
 	echo "csco0130@gmail.com";
    	echo "fmrk98@gmail.com";
         echo "csco013022@gmail.com";
