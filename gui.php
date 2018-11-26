e6fc0ee26cf2aee1e09c1fdceec02149945a2725
dockerdongjin <csco0130@gmail.com>
2018-11-26 11:27:07 +0900

    dongjin third

--- gui.php
+++ gui.php
@@ -1,8 +1,6 @@
 <?php
-<<<<<<< HEAD
 	echo "csco0130@gmail.com";
+   	echo "fmrk98@gmail.com"
+             echo "csco013022@gmail.com";
 ?>
-=======
-    echo "fmrk98@gmail.com"
-    ?>
->>>>>>> 9fc6a43536814b03730979266a512f9a81442794
+
